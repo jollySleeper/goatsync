19fd208198ef7b1217cc473ffcd385a97fcd1b6d
jollySleeper <git@pm21.anonaddy.com>
2025-02-22 09:01:15 +0530

    Started: GoatSync with Gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
     
 const banner = `
 --------------------------- Welcome To ------------------------------
@@ -15,4 +23,24 @@ const banner = `
 
 func main () {
     fmt.Println(banner)
+
+	engine := gin.Default()
+	engine.GET("/is_etebase", isEtebase)
+	
+    // Get port from environment variable or use default
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Printf("Using default port as dint find PORT in env")
+		port = "8080"
+	}
+
+	// Start the server
+	log.Printf("Server starting on port %s", port)
+	if err := engine.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+func isEtebase(context *gin.Context) {
+	context.Status(http.StatusOK)
 }
