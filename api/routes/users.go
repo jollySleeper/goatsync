a5ee31541b473858a21cb15cf81c8ec24744848e
jollySleeper <git@pm21.anonaddy.com>
2025-02-28 10:39:15 +0530

    Added: LoginChallenge API

--- api/routes/users.go
+++ api/routes/users.go
@@ -2,8 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+
+    "goatsync/internal/handlers"
 )
 
 func setupUserRoutes(rg *gin.RouterGroup) {
 	users := rg.Group("/users")
+    {
+        user.POST("/login_challenge", handlers.LoginChallenge)
+    }
 }
