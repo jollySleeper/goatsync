7dd854297308667744a93fb91f6c4fb6f3121cf5
jollySleeper <git@pm21.anonaddy.com>
2025-03-05 10:07:30 +0530

    Added: Response Structs for Other APIs

--- internal/models/response/user.go
+++ internal/models/response/user.go
@@ -5,3 +5,15 @@ type LoginChallengeResponse struct {
 	Challenge []byte `json:"challenge"`
 	Version   int    `json:"version"`
 }
+
+type UserResponse struct {
+	Username         string `json:"username"`
+	Email            string `json:"email"`
+	Pubkey           []byte `json:"pubkey"`
+	EncryptedContent []byte `json:"encryptedContent"`
+}
+
+type LoginResponse struct {
+	Token string       `json:"token"`
+	User  UserResponse `json:"user"`
+}
