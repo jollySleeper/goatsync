f51fd0f19aca89951bb1fed1de00aaeea5db9773
jollySleeper <git@pm21.anonaddy.com>
2025-03-05 10:07:02 +0530

    Added: Request Struct for Other APIs

--- internal/models/request/user.go
+++ internal/models/request/user.go
@@ -3,3 +3,32 @@ package request
 type LoginChallengeRequest struct {
 	Username string `json:"username" binding:"required"`
 }
+
+type LoginRequest struct {
+	Username  string `json:"username"`
+	Challenge []byte `json:"challenge"` // base64 encoded
+	Host      string `json:"host"`
+	Action    string `json:"action"`
+}
+
+type ChangePasswordRequest struct {
+	Username    string `json:"username"`
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+	Challenge   []byte `json:"challenge"` // base64 encoded
+	Host        string `json:"host"`
+	Action      string `json:"action"`
+}
+
+type SignUpRequest struct {
+	User             UserSignUpRequest `json:"user"`
+	Salt             []byte            `json:"salt"`
+	LoginPubkey      []byte            `json:"loginPubkey"`
+	Pubkey           []byte            `json:"pubkey"`
+	EncryptedContent []byte            `json:"encryptedContent"`
+}
+
+type UserSignUpRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
