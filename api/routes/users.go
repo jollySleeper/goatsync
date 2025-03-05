2c122fd9a87b65183f0cbea75dfbf9e5530512de
jollySleeper <git@pm21.anonaddy.com>
2025-03-05 10:08:27 +0530

    Implemented: APIs for User

--- api/routes/users.go
+++ api/routes/users.go
@@ -10,5 +10,10 @@ func setupUserRoutes(rg *gin.RouterGroup) {
 	users := rg.Group("/users")
     {
         user.POST("/login_challenge", handlers.LoginChallenge)
+        users.POST("/login", handlers.Login)
+		users.POST("/logout", handlers.Logout)
+		users.POST("/change_password", handlers.ChangePassword)
+		users.POST("/signup", handlers.SignUp)
+        // users.POST("/dashboard_url", handlers.DashboardURL)
     }
 }
